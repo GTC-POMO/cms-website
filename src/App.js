2deba9f2a97f631203aaa4fb9c7c2122d6b1310d
JacobMJones <theretherehuman@gmail.com>
2018-11-27 08:46:29 -0500

    can change background color, adds the start of page templates

--- src/App.js
+++ src/App.js
@@ -5,22 +5,29 @@ import siteData from "./Data/website-config.json";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {pageIndex:0};
+    this.state = { pageIndex: 0 };
   }
   componentDidMount() {
- console.log(siteData.pages)
+    console.log(siteData.pages);
   }
 
   advanceIndex = () => {
-
-  }
+    siteData.pages.length > this.state.pageIndex + 1
+      ? this.setState({ pageIndex: this.state.pageIndex + 1 })
+      : console.log("out of pages");
+  };
   render() {
-   const { title } = siteData.pages[this.state.pageIndex];
-   const {pageIndex} = this.state;
+    const { title, backgroundColor, pageType } = siteData.pages[this.state.pageIndex];
+    const { pageIndex } = this.state;
     return (
-      <div>
-         <h1 className="title">{title}</h1> 
-         <button onClick={()=>{this.setState({pageIndex:pageIndex + 1})}}/>
+      <div className='root-div' style={{backgroundColor:backgroundColor}}>
+        <h1 className="title" >{title}</h1>
+        <button
+        className='advance-button'
+          onClick={() => {
+            this.advanceIndex();
+          }}
+        />
       </div>
     );
   }
