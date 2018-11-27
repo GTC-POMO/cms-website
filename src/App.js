c1f42f705fd5ab328642dc0c96b130d02751137b
JacobMJones <theretherehuman@gmail.com>
2018-11-26 23:13:53 -0500

    initial commit, can read json created by cms and produce a basic title page, can advance through pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import siteData from "./Data/website-config.json";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {pageIndex:0};
+  }
+  componentDidMount() {
+ console.log(siteData.pages)
+  }
+
+  advanceIndex = () => {
+
+  }
   render() {
+   const { title } = siteData.pages[this.state.pageIndex];
+   const {pageIndex} = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+         <h1 className="title">{title}</h1> 
+         <button onClick={()=>{this.setState({pageIndex:pageIndex + 1})}}/>
       </div>
     );
   }
